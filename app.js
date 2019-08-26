0f2d9be24d431c794431c0b05f05b9848b7d38c7
Zachary <zlake23@gmail.com>
2019-08-25 20:36:26 -0700

    Adds text to display for win, lose or draw result

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const computerScore_span = document.getElementById('computer-score');
 const scoreBoard_div = document.querySelector(".scoreboard");
 const playerChoice_span = document.querySelector("#player-choice");
 const computerChoice_span = document.querySelector("#computer-choice");
+const gameResult_div = document.querySelector("#game-result-val");
 const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissors_div = document.getElementById("scissors");
@@ -26,6 +27,9 @@ function win(playerChoice, computerChoice) {
     computerScore_span.innerHTML = computerScore;
     playerChoice_span.innerHTML = "Player Chose: " + playerChoice;
     computerChoice_span.innerHTML = "Computer Chose: " + computerChoice;
+    gameResult_div.innerHTML = "Player Wins!";
+    document.getElementById("game-result-val").classList.remove('red-box', 'gray-box');
+    document.getElementById("game-result-val").classList.add('green-box');
 }
 
 function lose(playerChoice, computerChoice) {
@@ -34,6 +38,9 @@ function lose(playerChoice, computerChoice) {
     computerScore_span.innerHTML = computerScore;
     playerChoice_span.innerHTML = "Player Chose: " + playerChoice;
     computerChoice_span.innerHTML = "Computer Chose: " + computerChoice;
+    gameResult_div.innerHTML = "Player Loses!";
+    document.getElementById("game-result-val").classList.remove('green-box', 'gray-box');
+    document.getElementById("game-result-val").classList.add('red-box');
 }
 
 function draw(playerChoice, computerChoice) {
@@ -41,6 +48,9 @@ function draw(playerChoice, computerChoice) {
     computerScore_span.innerHTML = computerScore;
     playerChoice_span.innerHTML = "Player Chose: " + playerChoice;
     computerChoice_span.innerHTML = "Computer Chose: " + computerChoice;
+    gameResult_div.innerHTML = "It's a Draw!";
+    document.getElementById("game-result-val").classList.remove('red-box', 'green-box');
+    document.getElementById("game-result-val").classList.add('gray-box');
 }
 
 
@@ -69,6 +79,7 @@ function playGame(playerChoice) {
 function clearGame() {
     playerChoice_span.innerHTML = "Player Chose: ";
     computerChoice_span.innerHTML = "Computer Chose: ";
+    gameResult_div.innerHTML = "";
     playerScore = 0;
     playerScore_span.innerHTML = playerScore;
     computerScore = 0;
