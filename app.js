66d65b205de0ea4b10b8304a3a755c47642acc19
Zachary <zlake23@gmail.com>
2019-08-25 19:13:09 -0700

    Adds computerChoice function and playGame function

--- app.js
+++ app.js
@@ -10,12 +10,31 @@ const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissors_div = document.getElementById("scissors");
 
-rock_div.addEventListener('click', function() {
+
+function getComputerChoice() {
+    let choices = ['r', 'p', 's'];
+    const randomNumber = (Math.floor(Math.random() * 3));
+    return choices[randomNumber];
+}
+
+function playGame(userChoice) {
+    const computerChoice = getComputerChoice();
     
+    switch {
+        case:
+    }
+}
+
+function main() {
+rock_div.addEventListener('click', function() {
+    playGame("rock");
 });
 paper_div.addEventListener('click', function() {
-    
+   playGame("paper"); 
 });
 scissors_div.addEventListener('click', function() {
-    
+    playGame("scissors");
 });
+}
+
+main();
